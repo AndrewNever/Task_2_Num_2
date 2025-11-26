5d54c616161b7f29b1dd3a835ca59f93b4d1f5bd
nevermore <pavlenokandrey@gmail.com>
2025-11-26 19:00:17 +0300

    rework

--- src/main/java/com/cgvsu/rasterizationfxapp/RasterizationController.java
+++ src/main/java/com/cgvsu/rasterizationfxapp/RasterizationController.java
@@ -18,12 +18,21 @@ public class RasterizationController {
     @FXML
     private void initialize() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        // Очищаем canvas
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        // Рисуем РОВНЫЙ треугольник (равнобедренный)
         Rasterization.fillTriangleBarycentric(
                 gc,
-                100, 100, Color.RED,
-                400, 120, Color.GREEN,
-                220, 360, Color.BLUE
+                300, 100, Color.RED,    // Верхняя вершина по центру
+                150, 300, Color.GREEN,  // Левая нижняя вершина
+                450, 300, Color.BLUE    // Правая нижняя вершина
         );
-    }
 
+        // Рисуем линии алгоритмом Брезенхэма (один цвет для всей линии)
+//        Rasterization.drawLineBresenham(gc, 50, 50, 300, 100, Color.YELLOW);
+//        Rasterization.drawLineBresenham(gc, 300, 100, 200, 300, Color.PURPLE);
+//        Rasterization.drawLineBresenham(gc, 200, 300, 50, 50, Color.CYAN);
+    }
 }
